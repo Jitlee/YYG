6c432d7464222ed2751dd087965c9229de241d28
Jitlee <www.wpj@163.com>
2015-11-16 15:40:56 +0800

    添加主页tab页切换

--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -5,16 +5,20 @@
 		<meta name="author" content="m.178hui.com" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 		<meta name="format-detection" content="telephone=no">
+		<meta property="qc:admins" content="304107566762141336654" />
 		<title><?php echo ($title); ?></title>
 		<link href="/Public/Home/css/mui.min.css" rel="stylesheet" type="text/css" />
 		<link href="/Public/Home/css/owl.carousel.css" rel="stylesheet">
 		<link href="/Public/Home/css/global.css" rel="stylesheet" type="text/css" />
 		<link href="/Public/Home/css/public.css" rel="stylesheet" type="text/css" />
 		<link href="/Public/Home/css/index.css" rel="stylesheet" type="text/css" />
+		<link href="http://at.alicdn.com/t/font_1447654310_2816622.css" rel="stylesheet" type="text/css" />
+		<link href="/Public/Home/css/mobile.css" rel="stylesheet" type="text/css" />
 
 		<script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
 		<script src="/Public/Home/js/owl.carousel.min.js"></script>
 		<script src="/Public/Home/layer/layer.js"></script>
+		<script src="/Public/Home/js/mobile.js"></script>
 
 		<script type="text/javascript">
 			$(document).ready(function() {
@@ -98,28 +102,27 @@
 		</li>
 
 	</ul>
-<<<<<<< Updated upstream
 </div>
 		</div>
 		<nav class="mui-bar mui-bar-tab">
-			<a id="defaultTab" class="mui-tab-item mui-active" href="/index.php/Home/Index">
-				<span class="mui-icon mui-icon-home"></span>
+			<a id="home" class="mui-tab-item" href="<?php echo U('Home/index', '', '');?>">
+				<span class="mui-icon iconfont icon-yyg_home"></span>
 				<span class="mui-tab-label">首页</span>
 			</a>
-			<a class="mui-tab-item" href="/index.php/Home/MiaoSha">
-				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+			<a id="miaosha" class="mui-tab-item" href="<?php echo U('Miaosha/index', '', '');?>">
+				<span class="mui-icon iconfont icon-yyg_miaosha"><span class="mui-badge">9</span></span>
 				<span class="mui-tab-label">秒杀</span>
 			</a>
-			<a class="mui-tab-item" href="tab-webview-subpage-contact.html">
-				<span class="mui-icon mui-icon-contact"></span>
+			<a id="xiangou" class="mui-tab-item" href="<?php echo U('Xiangou/index', '', '');?>">
+				<span class="mui-icon iconfont icon-yyg_cart"></span>
 				<span class="mui-tab-label">限购</span>
 			</a>
-			<a class="mui-tab-item" href="tab-webview-subpage-setting.html">
-				<span class="mui-icon mui-icon-gear"></span>
+			<a id="paimai" class="mui-tab-item" href="<?php echo U('Paimai/index', '', '');?>">
+				<span class="mui-icon iconfont icon-yyg_paimai"></span>
 				<span class="mui-tab-label">拍卖</span>
 			</a>
-			<a class="mui-tab-item" href="/index.php/Home/Person/me">
-				<span class="mui-icon mui-icon-gear"></span>
+			<a id="person" class="mui-tab-item" href="<?php echo U('Person/me', '', '');?>">
+				<span class="mui-icon iconfont icon-yyg_me"></span>
 				<span class="mui-tab-label">我的</span>
 			</a>
 		</nav>
@@ -147,56 +150,7 @@
 				scrollTop: 0
 			});
 		});
+		
+		$("#<?php echo ((isset($pid) && ($pid !== ""))?($pid):'index'); ?>").addClass("mui-active");
 	});
-=======
-</div>
-		</div>
-		<nav class="mui-bar mui-bar-tab">
-			<a id="defaultTab" class="mui-tab-item mui-active" href="/index.php/Home/Index">
-				<span class="mui-icon mui-icon-home"></span>
-				<span class="mui-tab-label">首页</span>
-			</a>
-			<a class="mui-tab-item" href="/index.php/Home/MiaoSha">
-				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
-				<span class="mui-tab-label">秒杀</span>
-			</a>
-			<a class="mui-tab-item" href="tab-webview-subpage-contact.html">
-				<span class="mui-icon mui-icon-contact"></span>
-				<span class="mui-tab-label">限购</span>
-			</a>
-			<a class="mui-tab-item" href="tab-webview-subpage-setting.html">
-				<span class="mui-icon mui-icon-gear"></span>
-				<span class="mui-tab-label">拍卖</span>
-			</a>
-			<a class="mui-tab-item" href="/index.php/Home/Person/login">
-				<span class="mui-icon mui-icon-gear"></span>
-				<span class="mui-tab-label">我的</span>
-			</a>
-		</nav>
-		<div class="gotop backtop" style="display:none;"></div>
-	</body>
-
-</html>
-<script type="text/javascript">
-	//返回顶部
-	$(document).ready(function() {
-		$(window).scroll(function() {
-			var scrollHeight = $(document).height();
-			var scrollTop = $(window).scrollTop();
-			var $windowHeight = $(window).innerHeight();
-			scrollTop > 75 ? $(".gotop").fadeIn(200).css("display", "block") : $(".gotop").fadeOut(200).css({
-				"background-image": "url(/Public/Home/images/iconfont-fanhuidingbu.png)"
-			});
-		});
-		$('.backtop').click(function(e) {
-			$(".gotop").css({
-				"background-image": "url(/Public/Home/images/iconfont-fanhuidingbu_up.png)"
-			});
-			e.preventDefault();
-			$('html,body').animate({
-				scrollTop: 0
-			});
-		});
-	});
->>>>>>> Stashed changes
 </script>
