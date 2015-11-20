e4f3837f9a936a8710e289d75b8f80f3903929f9
ritacc <ritacc@126.com>
2015-11-20 23:53:11 +0800

    Signed-off-by: ritacc <ritacc@126.com>

--- Application/Home/Controller/OrderPayController.class.php
+++ Application/Home/Controller/OrderPayController.class.php
@@ -8,14 +8,44 @@ class OrderPayController extends Controller {
 public function Pay(){
     	$this->assign('title', '支付');
 		//$this->assign('pid', 'miaosha');
+		$arr=range(1,10000);
+		shuffle($arr);
+		$slides["rderNo"]='OY00001' . $arr[0];
+		$this->assign('data', $slides);
+		
         $this->display();
-}	
+}
+
+public function demo(){
+    	$this->assign('title', '支付');
+		//$this->assign('pid', 'miaosha');
+		$arr=range(1,10000);
+		shuffle($arr);
+		$slides["rderNo"]='OY00001' . $arr[0];
+		$this->assign('data', $slides);
+		
+        $this->display();
+}
+public function PayItem(){
+    	$this->assign('title', '支付');
+		//$this->assign('pid', 'miaosha');
+		$arr=range(1,10000);
+		shuffle($arr);
+		$slides["rderNo"]='OY00001' . $arr[0];
+		$this->assign('data', $slides);
+		
+        $this->display();
+}
 
 public function GetCharge()
 {
 	Pingpp::setApiKey('sk_test_48SSW5e1GqDKv9qnP8vLevLC');
 	try {
 		$orderNo="OY00001";
+		$arr=range(10000,99900);
+		shuffle($arr);
+		$orderNo=$orderNo . $arr[0];
+		$channel="alipay_wap";
 		$amount=0.01;
 	    $ch = \Pingpp\Charge::create(
 	        array(
