81fb92a499568e412e17781b7f330ab8d363310d
ritacc <ritacc@126.com>
2016-03-25 00:37:19 +0800

    Signed-off-by: ritacc <ritacc@126.com>

--- Application/P/Common/function.php
+++ Application/P/Common/function.php
@@ -52,7 +52,8 @@ function logger($log_content)
         $max_size = 500000;
         $log_filename = 'log.xml';
         if(file_exists($log_filename) and (abs(filesize($log_filename)) > $max_size)){unlink($log_filename);}
-        file_put_contents($log_filename, date('Y-m-d H:i:s').$log_content.'\r\n', FILE_APPEND);
+        file_put_contents($log_filename, date('Y-m-d H:i:s').$log_content.'
+        ', FILE_APPEND);
     }
 }
 
--- Application/P/Controller/PayController.class.php
+++ Application/P/Controller/PayController.class.php
@@ -58,38 +58,60 @@ class PayController extends \Home\Controller\PayController {
 		$message=$_POST["message"];
 		$signature=$_POST["signature"];
 		
-		logger($message);
+		vendor('jubaopay.jubaopay');
+		$jubaopay=new \jubaopay('jubaopay.ini');
+		$jubaopay->decrypt($message);
+		// 校验签名，然后进行业务处理
+		$result=$jubaopay->verify($signature);
+		if($result==1) {
+		   // 得到解密的结果后，进行业务处理
+		   $payid=$jubaopay->getEncrypt("payid");
+		   $state=$jubaopay->getEncrypt("state");
+		   $amount=$jubaopay->getEncrypt("amount");
+		   $orderNo=$jubaopay->getEncrypt("orderNo");
+		   $result="payid=	$payid	state=$state 	
+amount=$amount	orderNo=$orderNo ";
+		   if($state == '2')//成功
+			{
+			   	
+			}
+			else//失败
+			{
+			   		
+			}		   
+		    logger($result);
+			echo "success"; 	//向服务返回 "success"
+		}
+	}
+	public function back()
+	{
+		$message=$_GET["message"];
+		$signature=$_GET["signature"];
 		
 		vendor('jubaopay.jubaopay');
 		$jubaopay=new \jubaopay('jubaopay.ini');
 		$jubaopay->decrypt($message);
 		// 校验签名，然后进行业务处理
 		$result=$jubaopay->verify($signature);
-		logger($result);
 		if($result==1) {
 		   // 得到解密的结果后，进行业务处理
-		   // echo "payid=".$jubaopay->getEncrypt("payid")."<br />";
-		   // echo "mobile=".$jubaopay->getEncrypt("mobile")."<br />";
-		   // echo "amount=".$jubaopay->getEncrypt("amount")."<br />";
-		   // echo "remark=".$jubaopay->getEncrypt("remark")."<br />";
-		   // echo "orderNo=".$jubaopay->getEncrypt("orderNo")."<br />";
-		   // echo "state=".$jubaopay->getEncrypt("state")."<br />";
-		   // echo "partnerid=".$jubaopay->getEncrypt("partnerid")."<br />";
-		   // echo "modifyTime=".$jubaopay->getEncrypt("modifyTime")."<br />";
-		   logger("支付成功");
-			echo "success123"; // 像服务返回 "success"
-		} else {
-			echo "verify failed";
-			logger("verify failed");
+		   $payid=$jubaopay->getEncrypt("payid");
+		   $state=$jubaopay->getEncrypt("state");
+		   $amount=$jubaopay->getEncrypt("amount");
+		   $orderNo=$jubaopay->getEncrypt("orderNo");
+		   $result="payid=	$payid	state=$state 	
+amount=$amount	orderNo=$orderNo ";
+		   if($state == '2')//成功
+			{
+			   	
+			}
+			else//失败
+			{
+			   		
+			}		   
+		    logger($result);
+			echo "success"; 	//向服务返回 "success"
 		}
-
-		
-		echo 'back';
-	}
-	public function back()
-	{
-		layout(false);
-		$this->display();
 	}
 	public function paypc()
 	{
